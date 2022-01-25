47b15e58387333ecf00cb4da4237dbb3d5f7e989
Derlys <derlysr6@gmail.com>
2022-01-25 05:32:07 -0300

    Add first Activity with a layout xml file

---
+++ app/src/main/java/br/com/any/contraintlayout/ui/activity/PackageListActivity.kt
@@ -0,0 +1,12 @@
+package br.com.any.contraintlayout.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.any.contraintlayout.R
+
+class PackageListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_package_list)
+    }
+}
