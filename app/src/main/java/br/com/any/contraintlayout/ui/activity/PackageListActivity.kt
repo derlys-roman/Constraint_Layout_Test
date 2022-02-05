895f8b346ea6af240b088b3f096e9baecd19b25e
Derlys <derlysr6@gmail.com>
2022-02-05 06:48:25 -0300

    Set viewBindig to setContentView, add textView and new string resource

--- app/src/main/java/br/com/any/contraintlayout/ui/activity/PackageListActivity.kt
+++ app/src/main/java/br/com/any/contraintlayout/ui/activity/PackageListActivity.kt
@@ -2,11 +2,18 @@ package br.com.any.contraintlayout.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.inputmethod.InputBinding
 import br.com.any.contraintlayout.R
+import br.com.any.contraintlayout.databinding.ActivityPackageListBinding
 
 class PackageListActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityPackageListBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_package_list)
+        binding = ActivityPackageListBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
