ef675c754abb06a4715a3837f74764fc89fe760f
Derlys <derlysr6@gmail.com>
2022-03-03 06:27:43 -0300

    add new adapter

---
+++ app/src/main/java/br/com/any/contraintlayout/ui/adapter/PackageAdapter.kt
@@ -0,0 +1,28 @@
+package br.com.any.contraintlayout.ui.adapter
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class PackageAdapter(private val item : ArrayList<Any>) : RecyclerView.Adapter<PackageAdapter.PackageHolder>() {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PackageHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: PackageHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    class PackageHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        fun binding(){
+
+        }
+    }
+}
