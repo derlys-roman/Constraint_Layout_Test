9c2a8496f8bb08884ffc3e1279386fba58710408
Derlys <derlysr6@gmail.com>
2022-03-06 13:22:59 -0300

    start configure PackageAdapter

---
+++ app/src/main/java/br/com/any/contraintlayout/data/ItemDAO.kt
@@ -0,0 +1,50 @@
+package br.com.any.contraintlayout.data
+
+import br.com.any.contraintlayout.R
+import br.com.any.contraintlayout.model.ItemSources
+
+class ItemDAO {
+
+    private val itens: ArrayList<ItemSources> = arrayListOf(
+        ItemSources(
+            "São Paulo",
+            "@drawable/sao_paulo_sp",
+            "2 dias",
+            "R$ 243,99"
+        ),
+        ItemSources(
+            "Belo Horizonte",
+            "@drawable/belo_horizonte_mg",
+            "3 dias",
+            "R$ 421,50"
+        ),
+        ItemSources(
+            "Recife",
+            "@drawable/recife_pe",
+            "4 dias",
+            "R$ 754,20"
+        ),
+        ItemSources(
+            "Rio de Janeiro",
+            "@drawable/rio_de_janeiro_rj",
+            "6 dias",
+            "R$ 532,55"
+        ),
+        ItemSources(
+            "Salvador",
+            "@drawable/salvador_ba",
+            "5 dias",
+            "R$ 899,99"
+        ),
+        ItemSources(
+            "Foz do Iguaçu",
+            "@drawable/foz_do_iguacu_pr",
+            "1 dia",
+            "R$ 289,90"
+        ),
+    )
+
+    fun getAll(): ArrayList<ItemSources> {
+        return itens
+    }
+}
--- app/src/main/java/br/com/any/contraintlayout/ui/adapter/PackageAdapter.kt
+++ app/src/main/java/br/com/any/contraintlayout/ui/adapter/PackageAdapter.kt
@@ -1,26 +1,31 @@
 package br.com.any.contraintlayout.ui.adapter
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import br.com.any.contraintlayout.R
+import br.com.any.contraintlayout.databinding.ItemPackageBinding
+import br.com.any.contraintlayout.model.ItemSources
 
-class PackageAdapter(private val item: ArrayList<Any>) :
+class PackageAdapter(private val itemSources: ArrayList<ItemSources>) :
     RecyclerView.Adapter<PackageAdapter.PackageHolder>() {
 
+    private lateinit var binding: PackageHolder
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PackageHolder {
-        TODO("Not yet implemented")
+        val layoutInflater = LayoutInflater.from(parent.context)
+        return PackageHolder(layoutInflater.inflate(R.layout.item_package, parent, false))
     }
 
     override fun onBindViewHolder(holder: PackageHolder, position: Int) {
         TODO("Not yet implemented")
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
+    override fun getItemCount(): Int = itemSources.size
 
     class PackageHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
+        private val binding = ItemPackageBinding.bind(itemView)
         fun binding() {
 
         }
